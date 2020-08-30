7669dce1239456c629c33eaf458c422002d1f291
Masood Adeli <masood.adeli@cafebazaar.ir>
2020-08-31 00:37:55 +0430

    Adds fake pipeline

--- indexer.py
+++ indexer.py
@@ -1,3 +1,6 @@
+from faker import Faker
+
+
 class Task:
     task_name = 'task_name'
 
@@ -9,22 +12,61 @@ class Task:
 
 
 class Pipeline:
-    steps = []
-
     # single raw data provider
     def raw_data_provider(self):
+        # TODO: return generator
         raise NotImplementedError()
 
     def initiate(self, *tasks):
         self.steps = [*tasks]
 
-    def processed_data_handler(self):
+    def processed_data_handler(self, processed_data):
         raise NotImplementedError()
 
     def run(self):
-        raw_data = self.raw_data_provider()
-        processed_data = raw_data
-        for step in self.steps:
-            raw_data = processed_data
-            processed_data = step(processed_data)
-        self.processed_data_handler(processed_data)
+        # TODO: Add threading
+        continue_pipeline = True
+        while continue_pipeline:
+            raw_data = self.raw_data_provider()
+            if raw_data is None:
+                continue_pipeline = False
+                continue
+            processed_data = raw_data
+            for step in self.steps:
+                raw_data = processed_data
+                processed_data = step.run_task(processed_data)
+            self.processed_data_handler(processed_data)
+
+
+class FakeDataPipeline(Pipeline):
+    number_of_data = 0
+
+    def __init__(self, data_limit_count, **kwargs):
+        super().__init__(*kwargs)
+        self.data_limit_count = data_limit_count
+        self.fake = Faker()
+
+    def raw_data_provider(self):
+        if self.number_of_data >= self.data_limit_count:
+            return None
+        self.number_of_data += 1
+        return {
+            'text': self.fake.paragraph()
+        }
+
+    def processed_data_handler(self, processed_data):
+        print(processed_data)
+
+
+class TextLengthCalculatorTask(Task):
+    task_name = 'text_length_calculator_task'
+
+    def run_task(self, data):
+        data['length'] = len(data['text'])
+        return data
+
+
+text_length_calculator_task = TextLengthCalculatorTask()
+f = FakeDataPipeline(100)
+f.initiate(text_length_calculator_task)
+f.run()
